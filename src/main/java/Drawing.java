7a4cc1dc3406f8194b6156c7de24c29256e62f0d
gijsdepooter <gijsdepooter@gmail.com>
2021-10-24 17:30:04 +0100

    New superClass

--- src/main/java/Drawing.java
+++ src/main/java/Drawing.java
@@ -5,14 +5,31 @@ public class Drawing extends Canvas {
     // A private field called f of class Frame. This is also in the AWT.
     private Frame f;
     private Circle circ;
+    private Rect rect;
+    private Square sqr;
 
     // The constructor
     public Drawing() {
         Point p=new Point(200,200);
+        Point pr=new Point(100,100);
+        Point ps=new Point(100,300);
         Color c=new Color(0x992266);
 
-        circ = new Circle(p,c,30);
 
+        circ = new Circle(p,c,30);
+        rect = new Rect(pr,c,30,60);
+        sqr = new Square(ps,c,60);
+
+        window();
+        window_settings();
+    }
+
+    private void window_settings() {
+        setBackground(Color.WHITE); // Sets the Canvas background
+        setSize(400, 400); // Sets the Canvas size to be the same as the frame
+    }
+
+    private void window() {
         f = new Frame("My window"); // Instantiates the Frame
         f.add(this); // Adds the Canvas to the Frame
         f.setLayout(null); // Stops the frame from trying to layout contents
@@ -23,12 +40,12 @@ public class Drawing extends Canvas {
                 f.dispose();
             }
         });
-        setBackground(Color.WHITE); // Sets the Canvas background
-        setSize(400, 400); // Sets the Canvas size to be the same as the frame
     }
 
 
     public void paint(Graphics g){
         circ.draw(g);
+        rect.draw(g);
+        sqr.draw(g);
     }
 }
---
+++ src/main/java/Rect.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+public class Rect extends Shape {
+    private int width;
+    private int height;
+
+    public Rect(Point initPos,Color col, int w, int h){
+        super(initPos, col);
+        this.width = w;
+        this.height = h;
+    }
+
+    @Override
+    public void draw(Graphics g){
+        g.setColor(col);
+        g.fillRect(pos.x,pos.y,width,height);
+    }
+
+
+}
